c7cd3e4033be565ff1d2d9f9304d5de141392056
OAlasgarli08 <omarta@code.edu.az>
2023-09-13 22:18:22 +0400

    P418-HW4 & P418-CW

---
+++ P418-Classwork4/P418-Classwork4/Program.cs
@@ -0,0 +1,106 @@
+﻿// Number 1
+
+//int n = 10;
+
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("The Number is Even");
+//}
+//else
+//{
+//    Console.WriteLine("The number is Odd");
+//}
+
+// -------------------------------------------------------------------------
+
+// Number 2
+
+//int m = 10;
+
+//for (int i = 1;i<=10;i++)
+//{
+//    Console.WriteLine(i);
+//}    
+
+// -------------------------------------------------------------------------
+
+// Number 3
+
+//int m = 5;
+//int sum = 0;
+
+//for (int i = 1;i<=m;i++)
+//{
+//    sum += i;
+
+//}
+//Console.WriteLine(sum);
+
+// -------------------------------------------------------------------------
+
+// Number 4
+
+//int m = 10;
+//int count = 0;
+
+//for (int i = 1;i<=10;i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+// -------------------------------------------------------------------------
+
+// Number 5
+
+//int n = 2;
+//int m = 10;
+//int sum = 0;
+
+//for (int i = n; n < m; n++)
+//{
+//    if (n % 2 == 0)
+//    {
+//        sum+=n;
+//    }
+//}
+//Console.WriteLine(sum);
+
+// -------------------------------------------------------------------------
+
+// Number 6
+
+//int n = 5;
+//int multiple = 1;
+
+//if (n == 0)
+//{
+//    Console.WriteLine("Write a proper number");
+//}
+
+//for (int i = 1; i <= n; i++)
+//{
+//    multiple *= i;
+//}
+//Console.WriteLine(multiple);
+
+// -------------------------------------------------------------------------
+
+// Number 7
+
+//int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int multiple = 1;
+
+//for (int i = 0; i < number.Length; i++)
+//{
+//    if (number[i] % 2 == 0)
+//    {
+//        multiple *= number[i];
+//    }
+//}
+//Console.WriteLine(multiple);
+
+// -------------------------------------------------------------------------
---
+++ P418-HW4/P418-HW4/Program.cs
@@ -0,0 +1,106 @@
+﻿// Number 8
+
+//int n = 21;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("It is divisible by 3 and 7");
+//}
+//else
+//{
+//    Console.WriteLine("It is not divisible by 3 and 7");
+//}
+
+// -------------------------------------------------------------------------
+
+// NUmber 9
+
+//int n = 14;
+//int m = 16;
+
+//if(n%2==0 && m % 2 == 0)
+//{
+//    Console.WriteLine(n * m);
+//}
+//else
+//{
+//    Console.WriteLine("Put in another number");
+//}
+
+// -------------------------------------------------------------------------
+
+// Number 10
+
+//int n = 1;
+//int m = 5;
+//int sum = 0;
+
+//for (int i = n; i<m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.Write(sum);
+
+// -------------------------------------------------------------------------
+
+// Number 11
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int sum = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 1)
+//    {
+//        sum += arr[i];
+//    }
+//}
+//Console.WriteLine(sum);
+
+// -------------------------------------------------------------------------
+
+// Number 12
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int count = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+//}        
+//Console.WriteLine(count);
+
+// -------------------------------------------------------------------------
+
+// Number 13
+
+//int n = 10;
+//int count = 0;
+
+//if (n >= 1)
+//{
+//    Console.WriteLine("Type in a new number");
+//}
+
+//for (int i = 0; i < n; i++)
+//{
+//    if(n%i== 0)
+//    {
+//        count++;
+//    }
+//}
+
+//if (count == 2)
+//{
+//    Console.WriteLine("The number is simple");
+//}
+//else
+//{
+//    Console.WriteLine("The number is complex");
+//}
